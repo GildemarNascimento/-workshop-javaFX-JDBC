466c88699ae456887351679c1ff78ab1c1a1816d
GildemarNascimento <gildemargg@hotmail.com>
2021-02-16 11:11:34 -0300

    initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -31,18 +32,21 @@ public class MainViewController implements Initializable {
 	}
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	@FXML
 	public void onMenuItemAbountAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
 		
 	}
-	private synchronized  void loadView(String absoluteName) {
+	private synchronized  <T> void loadView(String absoluteName, Consumer<T> initialingAction	) {
 		//synchronized garante que processo multi theader não seja interrompido.
 		//Abria a About.
 		//Objeto carrega a tela.
@@ -62,38 +66,14 @@ public class MainViewController implements Initializable {
 			//Adicionar a Coleção menus About
 			mainVBox.getChildren().addAll(newVbox.getChildren());
 			
+			T controller = loader.getController();
+			initialingAction.accept(controller);
+			
 		} catch (IOException e) {
 			//Exibir o Alert.
 			Alerts.showAlert("IO Exception","Error loandig view", e.getMessage(), AlertType.ERROR);
 			
 		}
 	}	
-	private synchronized  void loadView2(String absoluteName) {
-		//synchronized garante que processo multi theader não seja interrompido.
-		//Abria a About.
-		//Objeto carrega a tela.
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVbox = loader.load();
-			//Pegar a referencia da cena do método da classe Main.
-			Scene  mainScene = Main.getMainScene();
-			//Pega ScrolPane\Content\VBox.
-			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
-			//Guardar uma referencia para o Menus.
-			Node mainMenu = mainVBox.getChildren().get(0); //pega o primeiro filho/posição.
-			//Limpa todos os filhos do VBox
-			mainVBox.getChildren().clear();
-			//Adcionar Menu do VBox
-			mainVBox.getChildren().add(mainMenu);
-			//Adicionar a Coleção menus About
-			mainVBox.getChildren().addAll(newVbox.getChildren());
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-		} catch (IOException e) {
-			//Exibir o Alert.
-			Alerts.showAlert("IO Exception","Error loandig view", e.getMessage(), AlertType.ERROR);
-			
-		}
-	}	
+	
 }
